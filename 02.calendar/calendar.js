d84e0e8e4b0335c2529133d690ae5d20bbcc3c58
Yuki Sato <yuki.sato@livesense.co.jp>
2025-06-02 10:52:28 +0900

    定数を使用するように

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,6 +3,8 @@
 import { DateTime } from "luxon";
 import minimist from "minimist";
 
+const WEEKDAYS_NUM = 7;
+
 const argv = minimist(process.argv.slice(2), {
   number: ["m", "y"],
   default: {
@@ -11,21 +13,23 @@ const argv = minimist(process.argv.slice(2), {
   },
 });
 
-const WEEKLINE_LENGTH = [...Array(7)].map(() => "01").join(" ").length;
+const weekline_length = [...Array(WEEKDAYS_NUM)]
+  .map(() => "01")
+  .join(" ").length;
 
 const firstLineStr = `${argv.m}月 ${argv.y}`;
-const bufferNum = Math.floor((WEEKLINE_LENGTH - firstLineStr.length) / 2);
+const bufferNum = Math.floor((weekline_length - firstLineStr.length) / 2);
 console.log(" ".repeat(bufferNum) + firstLineStr + " ".repeat(bufferNum));
 
 console.log("日 月 火 水 木 金 土");
 
 const firstDay = DateTime.local(argv.y, argv.m, 1);
 let currentDay = firstDay;
-let weekDays = [...Array(currentDay.weekday)].map(() => "  ");
+let weekDays = [...Array(currentDay.weekday % WEEKDAYS_NUM)].map(() => "  ");
 
 while (currentDay.ordinal < firstDay.plus({ month: 1 }).ordinal) {
   weekDays.push(String(currentDay.day).padStart(2, " "));
-  if (weekDays.length >= 7) {
+  if (weekDays.length >= WEEKDAYS_NUM) {
     console.log(weekDays.join(" "));
     weekDays = [];
   }
